484f789f7ddba56ec019d076e533116f13e6ed41
Austin Obimbo <obimboausts@gmail.com>
2025-02-08 17:43:02 +0300

    updated home page

--- js/custom.js
+++ js/custom.js
@@ -30,13 +30,14 @@ $(".owl-carousel").owlCarousel({
         }
     }
 });
+1°16'07.4"S 36°48'34.3"E
 
 /** google_map js **/
 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// function myMap() {
+//     var mapProp = {
+//         center: new google.maps.LatLng(40.712775, -74.005973),
+//         zoom: 18,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
